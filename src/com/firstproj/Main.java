7e93a3a80b702a03733814fe7d889ef87d2ce0b5
Archishmaan Upadhyaya <archishmaan.upa4@gmail.com>
2025-06-12 15:50:55 +0530

    Strings

--- src/com/firstproj/Main.java
+++ src/com/firstproj/Main.java
@@ -21,7 +21,28 @@ public class Main {
         boolean isAdult = true;
 
         // Non primitive
+
+        // String
+        // Strings are immutable in JAVA
         String name = "Archishmaan";  // It's not compulsory to write new for string
         System.out.println(name.length());
+
+        // concatenation
+        String friend1 = new String("Yatharth");
+        String friend2 = new String("Samarth");
+        String concat = name + ", " + friend1 + " and " + friend2;
+        System.out.println(concat);
+
+        // charAt
+        System.out.println(name.charAt(1));
+
+        // replace
+        String nameReplace = name.replace('A', 'O');
+        System.out.println(nameReplace);
+        System.out.println(name);
+
+        // substring
+        String substringCheck = concat.substring(13, 21);
+        System.out.println(substringCheck);
     }
 }
