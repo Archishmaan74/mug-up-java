86141b617cb7299464df6933fd58c46010100d17
Archishmaan Upadhyaya <archishmaan.upa4@gmail.com>
2025-06-12 14:28:07 +0530

    Initial commit

---
+++ src/com/firstproj/Main.java
@@ -0,0 +1,19 @@
+package com.firstproj;
+
+public class Main {
+    public static void main(String[] args) {
+        String name = "Archishmaan";
+        System.out.println(name);
+    }
+}
+
+// Types in JAVA
+/* byte - 1 bytes [-128 to 127]
+   short - 2 bytes
+   int - 4 bytes
+   long - 8 bytes [big int values]
+   float - 4 bytes
+   double - 8 bytes [decimals]
+   char - 2 bytes
+   boolean - 1 byte
+*/
